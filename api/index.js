190fc8e5e5e879defd579071eb3a4356eadae49d
Yağız Işkırık <y_iskirik_10@hotmail.com>
2022-09-26 20:32:43 +0300

    Adding customisation to text

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
-const createNewText = (color, text, fontSize) => {
+const createNewText = (color, text, fontSize, height, width, x, y) => {
   let t = `<svg version="1.1"
          xmlns="http://www.w3.org/2000/svg"
          xmlns:xlink="http://www.w3.org/1999/xlink"
+         height="${height}" width="${width}"
     >
-      <text font-size="${fontSize}" fill="#${color}">${text}</text>
+      <text x="${x}" y="${y}" font-size="${fontSize}" fill="#${color}">${text}</text>
     </svg>`;
 
   return t;
@@ -12,9 +13,13 @@ const createNewText = (color, text, fontSize) => {
 module.exports = (req, res) => {
   const color = req.query.color || "ff0000";
   const text = req.query.text || "";
-  const fontSize = parseInt(req.query.fs) || 16;
+  const fontSize = req.query.fontSize || "16";
+  const height = req.query.height || fontSize;
+  const y = req.query.y || fontSize;
+  const x = req.query.x || "0";
+  const width = req.query.width || "150";
 
-  const txt = createNewText(color, text, fontSize);
+  const txt = createNewText(color, text, fontSize, height, width, x, y);
   res.setHeader("Content-Type", "image/svg+xml");
   res.status(200).send(txt);
 };
